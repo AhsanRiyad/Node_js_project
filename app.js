7580c06f1b0581052932746d5836b41d8c347d60
AhsanRiyad <riyad298@gmail.com>
2019-03-07 22:40:55 +0600

    commit by alias
    this is used for short cut puspose

--- app.js
+++ app.js
@@ -8,17 +8,19 @@ var app = express();
 var port = 3000;
 
 //CONFIGURATION
-app.get('/' , (req,res)=>res.send('index page'));
+app.set('veiw engine' , 'ejs');
 
 
 
 //MIDDLEWARES
-
+app.use(exSession({secret: 'my top secret code', saveUninitialized: true, resave: false}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 
 
 //ROUTES
-
+app.get('/' , (req,res)=>res.send('index page'));
 
 
 //SERVER STARTUP
