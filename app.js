2d62847b39d87b1c820822c6e85a60815b71c56a
AhsanRiyad <riyad298@gmail.com>
2019-03-07 23:34:48 +0600

    bootstrap, jquery , main.js, style.css added

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var cookieParser = require('cookie-parser');
 var app = express();
 var port = 3000;
 
+
 //CONFIGURATION
-app.set('veiw engine' , 'ejs');
+app.set('view engine' , 'ejs');
 
 
 
@@ -16,11 +17,16 @@ app.set('veiw engine' , 'ejs');
 app.use(exSession({secret: 'my top secret code', saveUninitialized: true, resave: false}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-
+app.use('/assets/media', express.static('assets/media'));
+app.use('/assets/bootstrap/js', express.static('assets/bootstrap/js'));
+app.use('/assets/bootstrap/css', express.static('assets/bootstrap/css'));
+app.use('/assets/css', express.static('assets/css'));
+app.use('/assets/js', express.static('assets/js'));
+app.use('/assets/jquery', express.static('assets/jquery'));
 
 
 //ROUTES
-app.get('/' , (req,res)=>res.send('index page'));
+app.get('/' , (req,res)=>res.render('index'));
 
 
 //SERVER STARTUP
