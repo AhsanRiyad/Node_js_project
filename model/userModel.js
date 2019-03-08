c2a465e67f7aa0882f671425d6138837e65c9257
AhsanRiyad <riyad298@gmail.com>
2019-03-08 17:01:16 +0600

    table changed

--- model/userModel.js
+++ model/userModel.js
@@ -2,7 +2,7 @@ var db = require('./db');
 
 module.exports={	
 	validate: function(user, callback){
-		var sql = "select * from g_user where g_u_email='"+user.email+"' and g_u_password='"+user.password+"' ";
+		var sql = "select * from user where u_email='"+user.email+"' and u_password='"+user.password+"' ";
 
 		db.getResult(sql , callback);
 	},
