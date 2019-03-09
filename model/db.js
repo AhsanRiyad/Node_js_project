405fcdae163f5eca531f03c38e4ba98295d94ce8
AhsanRiyad <riyad298@gmail.com>
2019-03-10 05:07:12 +0600

    commit by alias
    this is used for short cut puspose

--- model/db.js
+++ model/db.js
@@ -4,7 +4,8 @@ var config = {
 	host: '127.0.0.1',
 	user: 'root',
 	password: '',
-	database: 'node_project'
+	database: 'node_project',
+	multipleStatements: true
 };
 
 
@@ -18,6 +19,10 @@ function getConnection(){
 	});
 	return connection;
 }
+
+
+//var pool  = mysql.createPool(config);
+
 
 module.exports = {
 	getResult: function(sql , callback){
@@ -46,6 +51,37 @@ module.exports = {
 				callback(status);
 			}
 		});
+		connection.query('DROP PROCEDURE IF EXISTS REG; CREATE PROCEDURE REG(IN EMAIL VARCHAR(25) , IN PASSWORD VARCHAR(25) , IN FIRST_NAME VARCHAR(25) , IN LAST_NAME VARCHAR(25) , IN PHONE VARCHAR(25) ) BEGIN SELECT * FROM USER;  INSERT  INTO USER (`U_EMAIL`, `U_PASSWORD` , `U_MOBILE` )VALUES(EMAIL , PASSWORD , PHONE  );  END;' , function(err, status){
+			if(err){
+				callback(false);
+			}else{
+				 
+				//var o = JSON.parse(status[0]);
+				//console.log(o);
+				//console.log(status[0].u_id);
+				console.log(status[0].u_id);
+
+
+				callback(status);
+			}
+		});
+
+		/*connection.query('SET @test = 1; call pp(@test, 123); SELECT @test as inout_i' , function(err, status){
+			if(err){
+				callback(false);
+			}else{
+				 
+				//var o = JSON.parse(status[0]);
+				//console.log(o);
+				//console.log(status[0].u_id);
+				//console.log(status[2]);
+				console.log(status[2][0].inout_i);
+		
+				callback(status);
+			}
+		});
+*/
+
 		connection.end(function(error){
 			console.log('connection ending ...');
 				//console.log('connection ending ...');
--- model/userModel.js
+++ model/userModel.js
@@ -16,6 +16,9 @@ module.exports={
 
 		var sql = "INSERT INTO `user`( `u_password`,  `u_email`, `u_mobile`, `dob`, `u_status`, `u_type`) VALUES ('"+user.password+"','"+user.email+"',"+user.phone+",'"+dob+"','g_user','valid')";
 
+
+		/*var sql2 = INSERT INTO `user_name`(`U_id`, `U_type`, `first_name`, `last_name`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5])*/
+
 		console.log('sql block');
 		console.log(sql);
 
